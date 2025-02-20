0d98eec37331c1a7b14aa4c77c29582189407be0
Zicoboii <jesusbygod2007@gmail.com>
2025-02-20 21:21:51 +0630

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ btn.addEventListener("click" , () => {
     navigator.geolocation.getCurrentPosition(postion => {
         const {latitude , longitude } = postion.coords;
         const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
-        fetch(url).then(res = res.json()).then(data => {
+        fetch(url).then(res => res.json()).then(data => {
             console.table(data.address);
         }).catch(() => {
             console.log(`Fetching Error from API`)
